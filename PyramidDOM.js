0eca42b495fd9a2c271ce6389c49267d97b6678d
AlexandruDr <draghicialex96@gmail.com>
2018-09-22 22:34:15 +0300

    Styled divs pyramid

---
+++ PyramidDOM.js
@@ -0,0 +1,32 @@
+function printPyramid(height) {
+ 
+    
+    
+   for ( var i = 0; i < height; i++)
+        {   
+            for ( var j = i; j < height-1; j++)
+                {
+                    
+                    var x = document.createElement("div");
+            x.innerHTML = '';
+            x.classList.add("select");
+            document.getElementById("pyramid").appendChild(x);
+                }
+            
+            for ( var t = 0; t<=i; t++)
+                {
+                    var y = document.createElement("div");
+            y.innerHTML = '';
+            y.classList.add("select1");
+            document.getElementById("pyramid").appendChild(y);
+                }
+            
+
+            var sep = document.createElement("div");
+            document.getElementById("pyramid").appendChild(sep);
+            
+        }
+    document.getElementById("construction").innerHTML = '';
+    
+    }
+printPyramid(7); 
